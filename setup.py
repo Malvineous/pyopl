5d5ef42f18c0b76ad356797d644d47d24953ca81
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2024-01-24 01:41:55 +1100

    Convert project to modern packaging tools.
    - Move metadata from setup.py into pyproject.toml
      - Add Adam Biser to the list of authors
      - Add myself (Laurence Dougal Myers) to the list of maintainers
      - Add minimum Python version of 2.7
      - Increase version to 1.4
    - Produce a wheel
    - setup.py: replace `distutils` with `setuptools`
    - README: convert to Markdown; add more contributor copyrights; update the example scripts.
    - .gitignore: ignore IDEA project dir, wheel build dirs, and venv (virtual environment) dir

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
-import os
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import Extension, setup
 
-# Borrowed from PyPI example
-def read(fname):
-	return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-setup(name='PyOPL',
-	version='1.3',
-	description='OPL2/3 Adlib emulation',
-	author='Adam Nielsen',
-	author_email='malvineous@shikadi.net',
-	url='http://www.github.com/Malvineous/pyopl',
-	license='GPL',
-	keywords='Adlib FM OPL OPL2 OPL3 YM3812 YMF262',
-	long_description=read('README'),
-	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: End Users/Desktop',
-		'License :: OSI Approved :: GNU General Public License (GPL)',
-		'Operating System :: OS Independent',
-		'Programming Language :: C++',
-		'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
-	],
-	platforms=['Any'],
+setup(
 	ext_modules=[Extension('pyopl', ['pyopl.cpp', 'dbopl.cpp'], depends=['dosbox.h', 'dbopl.h', 'adlib.h'])],
 )
